f310c1b80d1cb61499cf91aec80fdc950ff925f3
Amirhossein <156742248+AmirHosseinShahriyari@users.noreply.github.com>
2026-02-18 00:08:13 +0330

    add: CopyArrayManually Project

---
+++ CopyArrayManually/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get array length
+        System.out.print("Please enter array length :");
+        int arrayLength = scanner.nextInt();
+
+        int[] numbers = new int[arrayLength];
+
+        // Get array values
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.print("please enter index " + i + " value :");
+            numbers[i] = scanner.nextInt();
+        }
+
+        int[] copyNumbers = new int[arrayLength];
+
+        // Copy numbers in copyNumbers
+        for (int i = 0; i < arrayLength; i++) {
+            copyNumbers[i] = numbers[i];
+        }
+
+        // Result
+        System.out.println("Original :");
+        System.out.print("[ ");
+        for (int number : numbers){
+            System.out.print(number + ",");
+        }
+        System.out.print(" ]");
+
+        System.out.println();
+
+        System.out.println("Copy :");
+        System.out.print("[ ");
+        for (int number : copyNumbers){
+            System.out.print(number + ",");
+        }
+        System.out.print(" ]");
+
+
+
+    }
+    }
