c52167a3a2584766d533ba5714231b6ae6c77dbf
Amirhossein <156742248+AmirHosseinShahriyari@users.noreply.github.com>
2026-02-17 19:47:54 +0330

    add: SumOfArray Project

---
+++ SumOfArray/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get array length
+        System.out.print("please enter array length :");
+        int arrayLength = scanner.nextInt();
+
+        int[] numbers = new int[arrayLength];
+
+        // Get array values
+        for (int i = 0; i < arrayLength; i++){
+            System.out.print("please enter index " + i + " value :");
+            numbers[i] = scanner.nextInt();
+        }
+
+        // Calculating the sum of arrays
+        int sum = 0;
+        for (int number : numbers){
+            sum += number;
+        }
+        // Result
+        System.out.println("sum :" + sum);
+
+        }
+    }
