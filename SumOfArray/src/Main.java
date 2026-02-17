477282f3d797e7aebff0848d502f7a4d1a7bd3fc
Amirhossein <156742248+AmirHosseinShahriyari@users.noreply.github.com>
2026-02-17 20:28:06 +0330

    update: SumOfArray/src/Main.java

--- SumOfArray/src/Main.java
+++ SumOfArray/src/Main.java
@@ -4,25 +4,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get array length
-        System.out.print("please enter array length :");
-        int arrayLength = scanner.nextInt();
+        while (true) {
+            // Get array length
+            System.out.print("please enter array length :");
+            int arrayLength = scanner.nextInt();
 
-        int[] numbers = new int[arrayLength];
+            if(arrayLength == 0){
+                System.out.println("array length most be greater than 0");
+                continue;
+            }
 
-        // Get array values
-        for (int i = 0; i < arrayLength; i++){
-            System.out.print("please enter index " + i + " value :");
-            numbers[i] = scanner.nextInt();
+            int[] numbers = new int[arrayLength];
+
+            // Get array values
+            for (int i = 0; i < arrayLength; i++) {
+                System.out.print("please enter index " + i + " value :");
+                numbers[i] = scanner.nextInt();
+            }
+
+            // Calculating the sum of arrays
+            int sum = 0;
+            for (int number : numbers) {
+                sum += number;
+            }
+            // Result
+            System.out.println("sum :" + sum);
         }
-
-        // Calculating the sum of arrays
-        int sum = 0;
-        for (int number : numbers){
-            sum += number;
-        }
-        // Result
-        System.out.println("sum :" + sum);
 
         }
     }
