e12c65c7e288bab2417473e5b6bf65fa2f34c94a
Amirhossein <156742248+AmirHosseinShahriyari@users.noreply.github.com>
2026-02-17 20:28:48 +0330

    add: AverageOfArray/

---
+++ AverageOfArray/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Get array length
+            System.out.print("please enter array length :");
+            int arrayLength = scanner.nextInt();
+
+            if(arrayLength == 0){
+                System.out.println("array length most be greater than 0");
+                continue;
+            }
+
+            int[] numbers = new int[arrayLength];
+
+            // Get array values
+            for (int i = 0; i < arrayLength; i++) {
+                System.out.print("please enter index " + i + " value :");
+                numbers[i] = scanner.nextInt();
+            }
+
+            // Calculating the average of arrays
+            float average = 0;
+            for (int number : numbers) {
+                average += number;
+            }
+            average /= arrayLength;
+            System.out.println("average :" + average);
+        }
+        }
+    }
